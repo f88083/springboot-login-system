aefbd51760dd5b3fe69d80fdbba86337ea8632a5
f88083 <f88083@gmail.com>
2024-05-07 14:45:35 +0100

    Init.

---
+++ src/test/java/com/simonlai/springbootloginsystem/SpringbootLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonlai.springbootloginsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLoginSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
